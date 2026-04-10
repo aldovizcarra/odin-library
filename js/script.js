1f26d2c5e0b563adbc80a3f8db20dd4beadcee37
Aldo Vizcarra <avizcarra87@gmail.com>
2026-04-09 22:35:59 -0700

    Edit book constructor

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const myLibrary = [];
 
 function Book(options) {
+  this.options.id = options.id;
   this.options.title = options.title;
   this.options.author = options.author;
   this.options.pages = options.pages;
